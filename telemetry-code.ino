5a36031058299f373d75cf2fb755346590954037
spookysys <spookysys@gmail.com>
2016-10-03 18:52:50 +0200

    something strange - lost GPS

--- telemetry-code.ino
+++ telemetry-code.ino
@@ -1,41 +1,24 @@
 // NOTE: leftover code from arduino zero is hogging sercom5, which we need. Comment this out from variants.cpp to fix.
+#include <TinyGPS++.h>
 #include <Wire.h>
 #include <Uart.h>
 #include "wiring_private.h" // pinPeripheral() function
-
 #undef min
 #undef max
 #include <utility>
 #include <vector>
 
+
 // assert
 void assert_handler(const char* expr, const char* file, int line);
 #define assert(expr) do { if (!(expr)) { assert_handler(#expr, __FILE__, __LINE__); } } while(0)
+
     
 // debug mode
 #define DEBUG
 
  
 
-
-
-extern "C" char *sbrk(int i);
-namespace misc 
-{
-  float readBatteryVoltage()
-  {
-    static const auto VBAT_PIN = A7;
-    static const float ref_volt = 3.0f;
-    return analogRead(VBAT_PIN)*(ref_volt*2.f/1024.f);
-  }
-
-  int freeRam () 
-  {  
-    char stack_dummy = 0;  
-    return &stack_dummy - sbrk(0); 
-  }
-
-}
 
 
 
@@ -98,6 +81,26 @@ void assert_handler(const char* expr, const char* file, int line)
 
 
 
+extern "C" char *sbrk(int i);
+namespace misc 
+{
+  float readBatteryVoltage()
+  {
+    static const auto VBAT_PIN = A7;
+    static const float ref_volt = 3.0f;
+    return analogRead(VBAT_PIN)*(ref_volt*2.f/1024.f);
+  }
+
+  int freeRam () 
+  {  
+    char stack_dummy = 0;  
+    return &stack_dummy - sbrk(0); 
+  }
+
+}
+
+
+
 // Allow out-of-order processing of received messages and stuff
 class SerialComm
 {  
@@ -106,13 +109,11 @@ class SerialComm
   long long tick=0;
   static const int timeout = 10;
   Uart& my_serial;
-  const char* n;
   bool start_of_line = true;
 public:
-  SerialComm(const char* n, Uart& my_serial) : n(n), my_serial(my_serial) {}
+  SerialComm(Uart& my_serial) : my_serial(my_serial) {}
 
-  void IrqHandler()
-  {
+  void IrqHandler() {
     my_serial.IrqHandler();
   }
   
@@ -124,7 +125,6 @@ public:
       if (ch == '\n') {
         curStr.trim();
         if (curStr.length()>0) {
-          logger.print(n);
           logger.print("<");
           logger.println(curStr);
           received.push_back(std::make_pair(timeout, curStr));
@@ -154,6 +154,8 @@ public:
         match = iter->second.startsWith(str);
       }
       if (match) {
+        logger.print("Purged: ");
+        logger.println(iter->second);
         iter = received.erase(iter);
       } else {
         iter++;
@@ -171,11 +173,9 @@ public:
       } else {
         match = iter->second.startsWith(str);
       }
-
       if (match) {
         if (retStr) *retStr = iter->second;
-        logger.print(n);
-        logger.print("=");
+        logger.print("Match: ");
         logger.println(iter->second);
         if (found) logger.println("Warning: Multiple matches!");
         iter = received.erase(iter);
@@ -187,14 +187,10 @@ public:
     return found;
   }
 
-
   template<typename... Args>
   void print(Args&&... args)
   {
-    if (start_of_line) {
-      logger.print(n);
-      logger.print(">");
-    }
+    if (start_of_line) logger.print(">");
     logger.print(std::forward<Args>(args)...);
     my_serial.print(std::forward<Args>(args)...);
     start_of_line = false;
@@ -203,10 +199,7 @@ public:
   template<typename... Args>
   void println(Args&&... args)
   {
-    if (start_of_line) {
-      logger.print(n);
-      logger.print(">");
-    }
+    if (start_of_line) logger.print(">");
     logger.println(std::forward<Args>(args)...);
     my_serial.println(std::forward<Args>(args)...);
     start_of_line = true;
@@ -215,27 +208,18 @@ public:
   template<typename... Args>
   void write(Args&&... args)
   {
-    if (start_of_line) {
-      logger.print(n);
-      logger.print(">");
-    }
+    if (start_of_line) logger.print(">");
     logger.write(std::forward<Args>(args)...);
     my_serial.write(std::forward<Args>(args)...);
     start_of_line = false;
   }
 
-  void flush()
-  {
-    logger.print(n);
-    logger.println(".flush()");
-    my_serial.flush();
-  }
 };
 
 
 
 
-class GsmComm : public SerialComm
+class Gsm : public SerialComm
 {
   static const auto BAUD = 9600;
   static const auto PIN_RX = 3ul; // PA09 // SERCOM2.1 // Their GSM_TX
@@ -244,20 +228,20 @@ class GsmComm : public SerialComm
   static const auto PIN_CTS = 5ul; // PA15 // SERCOM2.3 
   static const auto PAD_RX = SERCOM_RX_PAD_1; // Use pad 1 for RX
   static const auto PAD_TX = UART_TX_RTS_CTS_PAD_0_2_3; // UART_TX_PAD_0 or UART_TX_RTS_CTS_PAD_0_2_3  
-  Uart serial;
+  Uart ss;
 public:
-  GsmComm() : serial(&sercom2, PIN_RX, PIN_TX, PAD_RX, PAD_TX), SerialComm("gsm", serial) {}
+  Gsm() : ss(&sercom2, PIN_RX, PIN_TX, PAD_RX, PAD_TX), SerialComm(ss) {}
   
   void begin() {
     logger.println("GSM?");
 
     // bring up serial
-    serial.begin(BAUD);
+    ss.begin(BAUD);
     pinPeripheral(PIN_TX, PIO_SERCOM_ALT);
     pinPeripheral(PIN_RX, PIO_SERCOM_ALT);
     pinPeripheral(PIN_RTS, PIO_SERCOM);
     pinPeripheral(PIN_CTS, PIO_SERCOM);
-    while (!serial);
+    while (!ss);
 
     // calibrate baud
     while(1) {
@@ -283,17 +267,17 @@ public:
     
     logger.println("GSM!");
   }
-} gsmComm;
+} gsm;
 
 void SERCOM2_Handler()
 {
-  gsmComm.IrqHandler();
+  gsm.IrqHandler();
 }
 
 
 
 
-class GpsComm : public SerialComm
+class Gps : public TinyGPSPlus
 {
   static const auto BAUD = 115200;
   static const auto PIN_EN = 26; // PA27
@@ -301,52 +285,90 @@ class GpsComm : public SerialComm
   static const auto PIN_TX = 30; // PB22 // SERCOM5.2 // Their GPS_RX
   static const auto PAD_RX = SERCOM_RX_PAD_3;
   static const auto PAD_TX = UART_TX_PAD_2;
-  Uart serial;
+  Uart ss;
 public:
-  GpsComm() : serial(&sercom5, PIN_RX, PIN_TX, PAD_RX, PAD_TX), SerialComm("gps", serial) {}
-  
+  Gps() : TinyGPSPlus(), ss(&sercom5, PIN_RX, PIN_TX, PAD_RX, PAD_TX){}
+
+  void IrqHandler() {
+    ss.IrqHandler();
+  }
+
   void begin() {
     logger.println("GPS?");
 
     // Enable GNSS
-//    pinMode(PIN_EN, INPUT_PULLDOWN);
-//    delay(1000);
-//    pinMode(PIN_EN, INPUT_PULLUP);
-//    delay(1000);
-
-    // Bring up serial
-    serial.begin(BAUD);
-    pinPeripheral(PIN_TX, PIO_SERCOM_ALT);
-    pinPeripheral(PIN_RX, PIO_SERCOM_ALT);
-    while (!serial);
-
-
-    // Enable GNSS
+    pinMode(PIN_EN, INPUT_PULLUP);
     delay(1000);
     pinMode(PIN_EN, INPUT_PULLDOWN);
     delay(1000);
     pinMode(PIN_EN, INPUT_PULLUP);
     delay(1000);
-          
-    // calibrate baud
-    /*
-    while(1) {
-      println("AT");
-      delay(100); 
-      update();
-      if (match(nullptr, "OK")) 
-        break;
-    }
-    purge("AT");
-    */
+    
+    // Bring up serial
+    ss.begin(BAUD);
+    pinPeripheral(PIN_TX, PIO_SERCOM_ALT);
+    pinPeripheral(PIN_RX, PIO_SERCOM_ALT);
+    while (!ss);
 
+    // Enable GNSS
+    pinMode(PIN_EN, INPUT_PULLUP);
+    delay(1000);
+    pinMode(PIN_EN, INPUT_PULLDOWN);
+    delay(1000);
+    pinMode(PIN_EN, INPUT_PULLUP);
+    delay(1000);
+    
     logger.println("GPS!");
   }
-} gpsComm;
+
+  void update() {
+    while (ss.available()) {
+      char ch = ss.read();
+      logger.print(ss);
+      encode(ch);
+    }
+  }
+
+  void dumpAll()
+  {
+    logger.println(location.lat(), 6); // Latitude in degrees (double)
+    logger.println(location.lng(), 6); // Longitude in degrees (double)
+    logger.print(location.rawLat().negative ? "-" : "+");
+    logger.println(location.rawLat().deg); // Raw latitude in whole degrees
+    logger.println(location.rawLat().billionths);// ... and billionths (u16/u32)
+    logger.print(location.rawLng().negative ? "-" : "+");
+    logger.println(location.rawLng().deg); // Raw longitude in whole degrees
+    logger.println(location.rawLng().billionths);// ... and billionths (u16/u32)
+    logger.println(date.value()); // Raw date in DDMMYY format (u32)
+    logger.println(date.year()); // Year (2000+) (u16)
+    logger.println(date.month()); // Month (1-12) (u8)
+    logger.println(date.day()); // Day (1-31) (u8)
+    logger.println(time.value()); // Raw time in HHMMSSCC format (u32)
+    logger.println(time.hour()); // Hour (0-23) (u8)
+    logger.println(time.minute()); // Minute (0-59) (u8)
+    logger.println(time.second()); // Second (0-59) (u8)
+    logger.println(time.centisecond()); // 100ths of a second (0-99) (u8)
+    logger.println(speed.value()); // Raw speed in 100ths of a knot (i32)
+    logger.println(speed.knots()); // Speed in knots (double)
+    logger.println(speed.mph()); // Speed in miles per hour (double)
+    logger.println(speed.mps()); // Speed in meters per second (double)
+    logger.println(speed.kmph()); // Speed in kilometers per hour (double)
+    logger.println(course.value()); // Raw course in 100ths of a degree (i32)
+    logger.println(course.deg()); // Course in degrees (double)
+    logger.println(altitude.value()); // Raw altitude in centimeters (i32)
+    logger.println(altitude.meters()); // Altitude in meters (double)
+    logger.println(altitude.miles()); // Altitude in miles (double)
+    logger.println(altitude.kilometers()); // Altitude in kilometers (double)
+    logger.println(altitude.feet()); // Altitude in feet (double)
+    logger.println(satellites.value()); // Number of satellites in use (u32)
+    logger.println(hdop.value()); // Horizontal Dim. of Precision (100ths-i32)
+  }
+
+} gps;
 
 void SERCOM5_Handler()
 {
-  gpsComm.IrqHandler();
+  gps.IrqHandler();
 }
 
 
@@ -400,9 +422,6 @@ public:
     delay(800);
     powerOnOff();
     assert(isOn());
-
-    gpsComm.begin();
-    gsmComm.begin();
    
     logger.println("SimCom!");
   }
@@ -419,6 +438,8 @@ void setup() {
   
   logger.begin();
   simCom.begin();
+  gps.begin();
+  gsm.begin();
 
   
   logger.println("Setup!");
@@ -427,12 +448,12 @@ void setup() {
 
 
 void loop() {
+  gsm.update();
+  gps.update();
+  //gps.dumpAll();
   logger.print("VBat: " ); 
   logger.println(misc::readBatteryVoltage());
   logger.print("Ram: ");
   logger.println(misc::freeRam());
-  gsmComm.update();
-  gpsComm.update();
-  //gpsComm.println("AT+CGNSPWR=?");
   delay(1000);
 }
