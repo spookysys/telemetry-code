ccf88f2e23b8f7ef7a403001c0b77ce7efcee449
Eivind Liland <spookysys@gmail.com>
2017-01-07 14:56:46 +0100

    sensor-calib.py parses json input

--- sensor-calibration/sensor-calibration.py
+++ sensor-calibration/sensor-calibration.py
@@ -2,8 +2,15 @@
 
 import sys
 import json
+from pprint import pprint
 import cal_lib
 
 
 print('Number of arguments:', len(sys.argv), 'arguments.')
 print('Argument List:', str(sys.argv))
+
+filename = sys.argv[1]
+with open(filename) as jsonfile:
+    data = json.load(jsonfile)
+
+print(data)
