827415133ca959ae7ff59bd3f902c7931a5f87e4
Eivind Liland <spookysys@gmail.com>
2017-01-07 14:37:07 +0100

    Cleanups related to python code - can now debug

--- sensor-calibration/cal_lib.py
+++ sensor-calibration/cal_lib.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python3
 """
 cal_lib.py - Ellipsoid into Sphere calibration library based upon numpy and linalg
-Copyright (C) 2012 Fabio Varesano 
+Copyright (C) 2012 Fabio Varesano
 
 Development of this code has been supported by the Department of Computer Science,
 Universita' degli Studi di Torino, Italy within the Piemonte Project
@@ -24,39 +25,40 @@ along with this program.  If not, see .
 import numpy
 from numpy import linalg
 
+
 def calibrate(x, y, z):
-  H = numpy.array([x, y, z, -y**2, -z**2, numpy.ones([len(x), 1])])
-  H = numpy.transpose(H)
-  w = x**2
-  
-  (X, residues, rank, shape) = linalg.lstsq(H, w)
-  
-  OSx = X[0] / 2
-  OSy = X[1] / (2 * X[3])
-  OSz = X[2] / (2 * X[4])
-  
-  A = X[5] + OSx**2 + X[3] * OSy**2 + X[4] * OSz**2
-  B = A / X[3]
-  C = A / X[4]
-  
-  SCx = numpy.sqrt(A)
-  SCy = numpy.sqrt(B)
-  SCz = numpy.sqrt(C)
-  
-  return ([OSx, OSy, OSz], [SCx, SCy, SCz])
+    H = numpy.array([x, y, z, -y**2, -z**2, numpy.ones([len(x), 1])])
+    H = numpy.transpose(H)
+    w = x**2
+
+    (X, residues, rank, shape) = linalg.lstsq(H, w)
+
+    OSx = X[0] / 2
+    OSy = X[1] / (2 * X[3])
+    OSz = X[2] / (2 * X[4])
+
+    A = X[5] + OSx**2 + X[3] * OSy**2 + X[4] * OSz**2
+    B = A / X[3]
+    C = A / X[4]
+
+    SCx = numpy.sqrt(A)
+    SCy = numpy.sqrt(B)
+    SCz = numpy.sqrt(C)
+
+    return ([OSx, OSy, OSz], [SCx, SCy, SCz])
 
 
 if __name__ == "__main__":
-  acc_f = open("acc.txt", 'r')
-  acc_x = []
-  acc_y = []
-  acc_z = []
-  for line in acc_f:
-    reading = line.split()
-    acc_x.append(int(reading[0]))
-    acc_y.append(int(reading[1]))
-    acc_z.append(int(reading[2]))
+    acc_f = open("acc.txt", 'r')
+    acc_x = []
+    acc_y = []
+    acc_z = []
+    for line in acc_f:
+        reading = line.split()
+        acc_x.append(int(reading[0]))
+        acc_y.append(int(reading[1]))
+        acc_z.append(int(reading[2]))
 
-  (offsets, scale) = calibrate(numpy.array(acc_x), numpy.array(acc_y), numpy.array(acc_z))
-  print offsets
-  print scale
+    (offsets, scale) = calibrate(numpy.array(acc_x), numpy.array(acc_y), numpy.array(acc_z))
+    print(offsets)
+    print(scale)
---
+++ sensor-calibration/sensor-calibration.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+import cal_lib
+
+
+print('Number of arguments:', len(sys.argv), 'arguments.')
+print('Argument List:', str(sys.argv))
