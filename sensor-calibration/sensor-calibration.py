344ea1a2ada87360f628925518bce04003a31ae6
Eivind Liland <spookysys@gmail.com>
2017-01-08 14:43:17 +0100

    gl visualization working (for debugging)

--- sensor-calibration/sensor-calibration.py
+++ sensor-calibration/sensor-calibration.py
@@ -8,8 +8,9 @@ from pprint import pprint
 import numpy as np
 import cal_lib
 from transforms3d import axangles
-from rigid_transform_3D import rigid_rotation_3D, rigid_transform_3D
-
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+from OpenGL.GL import *
 
 def load_input():
     if len(sys.argv) != 2:
@@ -46,12 +47,9 @@ def analyze_mag_accel(vecs):
 
 def analyze_gyro(accel_fitted, mag_fitted, gyro_raw):
     assert len(gyro_raw) == len(accel_fitted)
-    assert len(accel_fitted) == len(mag_fitted, gyro_raw)
-
-    for i in range(len(gyro_raw)-1):
-        
-
-
+    assert len(accel_fitted) == len(mag_fitted)
+    # for i in range(len(gyro_raw)-1):
+    return None
 
 accel_raw, mag_raw, gyro_raw = load_input()
 
@@ -63,3 +61,77 @@ mag_fitted = [adjust(x, mag_fit['offset'], mag_fit['scale']) for x in mag_raw]
 
 gyro_fit = analyze_gyro(accel_fitted, mag_fitted, gyro_raw)
 
+
+
+
+
+gl_view_rotate = np.array([0, 0])
+def gl_display():
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+    glPushMatrix()
+
+    gluLookAt(0, 0, 5,
+              0, 0, 0,
+              0, 1, 0)
+
+    mag = np.linalg.norm(gl_view_rotate)
+    if mag > 1:
+        scaler = 3
+        axe = gl_view_rotate / mag
+        glRotate(mag/scaler, axe[1], axe[0], 0)
+
+    color = [1, 1, 1]
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, color)
+    glutWireCube(2)
+    
+    glPopMatrix()
+    glutSwapBuffers()
+    return
+
+gl_mouse_last_coord = [0, 0]
+def gl_mouse_button(button, state, x, y):
+    global gl_mouse_last_coord
+    if button == 2:
+        sys.exit(0)
+    gl_mouse_last_coord = [x, y]
+
+def gl_mouse_motion(x, y):
+    global gl_view_rotate, gl_mouse_last_coord
+    gl_view_rotate += np.array([x, y]) - gl_mouse_last_coord
+    gl_mouse_last_coord = [x, y]
+
+def gl_idle():
+    glutPostRedisplay()
+
+def gl_main():
+    glutInit(sys.argv)
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
+    glutInitWindowSize(400, 400)
+    glutCreateWindow(b"Visualize")
+
+    glClearColor(0., 0., 0., 1.)
+
+    glShadeModel(GL_SMOOTH)
+    glEnable(GL_CULL_FACE)
+    glEnable(GL_DEPTH_TEST)
+    glEnable(GL_LIGHTING)
+    lightZeroPosition = [10., 4., 10., 1.]
+    lightZeroColor = [0.8, 1.0, 0.8, 1.0] #green tinged
+    glLightfv(GL_LIGHT0, GL_POSITION, lightZeroPosition)
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, lightZeroColor)
+    glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 0.1)
+    glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.05)
+    glEnable(GL_LIGHT0)
+    glutDisplayFunc(gl_display)
+    glutMotionFunc(gl_mouse_motion)
+    glutMouseFunc(gl_mouse_button)
+    glutIdleFunc(gl_idle)
+    glMatrixMode(GL_PROJECTION)
+    gluPerspective(40., 1., 1., 40.)
+    glMatrixMode(GL_MODELVIEW)
+    glutMainLoop()
+    return
+
+
+if __name__ == '__main__':
+    gl_main()
