dd9a052aaeaff1aed818683aa7915b88dfda823f
Eivind Liland <spookysys@gmail.com>
2017-01-14 18:18:14 +0100

    "

--- telemetry-code.ino
+++ telemetry-code.ino
@@ -10,43 +10,42 @@
 
 
 namespace {
-  Stream& logger = Serial;
+	Stream& logger = Serial;
 
-  float readBatteryVoltage()
-  {
-	static const auto VBAT_PIN = A7;
-	static const float ref_volt = 3.0f;
-	return analogRead(VBAT_PIN) * (ref_volt * 2.f / 1024.f);
-  }
-  
-  extern "C" char *sbrk(int i);
-  inline unsigned long freeRam ()
-  {
-	char stack_dummy = 0;
-	return &stack_dummy - sbrk(0);
-  }
+	float readBatteryVoltage()
+	{
+		static const auto VBAT_PIN = A7;
+		static const float ref_volt = 3.0f;
+		return analogRead(VBAT_PIN) * (ref_volt * 2.f / 1024.f);
+	}
 
-  void wireKhz(int wire_khz)
-  {
-	sercom3.disableWIRE();
-	SERCOM3->I2CM.BAUD.bit.BAUD = SystemCoreClock / (2000 * wire_khz) - 1;
-	sercom3.enableWIRE();        
-  }
+	extern "C" char *sbrk(int i);
+	inline unsigned long freeRam ()
+	{
+		char stack_dummy = 0;
+		return &stack_dummy - sbrk(0);
+	}
 
-
-  events::Process& blink_process = events::makeProcess("blink").subscribe([&](unsigned long time, unsigned long delta) {
-	static bool val = false;
-	pinMode(pins::LED, OUTPUT);
-	digitalWrite(pins::LED, val);    
-	val = !val;
-  });
+	void wireKhz(int wire_khz)
+	{
+		sercom3.disableWIRE();
+		SERCOM3->I2CM.BAUD.bit.BAUD = SystemCoreClock / (2000 * wire_khz) - 1;
+		sercom3.enableWIRE();        
+	}
 
 
+	events::Process& blink_process = events::makeProcess("blink").subscribe([&](unsigned long time, unsigned long delta) {
+		static bool val = false;
+		pinMode(pins::LED, OUTPUT);
+		digitalWrite(pins::LED, val);    
+		val = !val;
+	});
 
-  events::Process& stats_process = events::makeProcess("stats").subscribe([&](unsigned long time, unsigned long delta) {
-	//logger.println(String() + "Free RAM: " + freeRam() + ", Voltage: " + readBatteryVoltage());
-  }).setPeriod(10000);
 
+
+	events::Process& stats_process = events::makeProcess("stats").subscribe([&](unsigned long time, unsigned long delta) {
+		//logger.println(String() + "Free RAM: " + freeRam() + ", Voltage: " + readBatteryVoltage());
+	}).setPeriod(10000);
 }
 
 
